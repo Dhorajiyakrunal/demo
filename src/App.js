4e25c4cee1de29271f0163c14642a66dedc4f164
Dhorajiyakrunal <111419915+Dhorajiyakrunal@users.noreply.github.com>
2022-12-08 09:07:14 +0530

    update Text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          HEllo React
+          Krunal Dhorajiya
         </a>
       </header>
     </div>
